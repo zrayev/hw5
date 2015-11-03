2e9478dcab93f0e941e60b6236fd35dd58f08584
dmitry <zrayev@mail.ru>
2015-11-04 00:17:13 +0200

    tests v1

---
+++ Tests/CalculatorTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 03.11.15
+ * Time: 22:42
+ */
+namespace Calculator;
+
+require_once 'vendor/autoload.php';
+use Calculator;
+//use PHPUnit_Framework_TestCase;
+
+class CalculatorTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider addProvider
+     * @param $ex
+     * @param $a
+     * @param $b
+     */
+    public function testAdd($ex, $a, $b)
+    {
+        $calc = new Calculator();
+        $this->assertEquals($ex, $calc->add($a, $b));
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @dataProvider multiplProvider
+     */
+    public function testMultipl($ex, $a, $b)
+    {
+        $calc = new Calculator();
+        $this->assertEquals($ex, $calc->multipl($a, $b));
+    }
+
+    public function addProvider()
+    {
+        return [
+            [6, 3, 3],
+            [4, 2, 2],
+            [8, 5, 3],
+            [9, 3, 6],
+        ];
+    }
+
+    public function multiplProvider()
+    {
+        return [
+            [6, 2, 3],
+            [10, 2, 5],
+            [12, 6, 2],
+        ];
+    }
+}
---
+++ src/Calculator.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 03.11.15
+ * Time: 22:41
+ */
+namespace Calculator {
+    class Calculator
+    {
+      //  public $a = 2;
+       // public $b = 3;
+
+        public function add($a, $b)
+        {
+            return $a + $b;
+        }
+
+        public function multipl($a, $b)
+        {
+            return $a * $b;
+        }
+    }
+}
