bdb432d99c5eed2eef6074f5b05879f2fce4e8a9
dmitry <zrayev@mail.ru>
2015-11-04 14:38:42 +0200

    v2

---
+++ src/Tests/GeronTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 04.11.15
+ * Time: 12:52
+ */
+require_once 'vendor/autoload.php';
+
+use Geron\Geron;
+
+class GeronTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $ex
+     * @param $a
+     * @param $b
+     * @param $c
+     */
+    public function TestCalcP($ex, $a, $b, $c)
+    {
+        /** @var TYPE_NAME $calc1 */
+        $calc1 = new Geron();
+        $this->assertEquals($ex, $calc1->calcP($a, $b, $c));
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @return array
+     */
+    public function calcPProvider()
+    {
+        return [
+            [12, 2, 3, 7 ],
+            [12, 2, 4, 6],
+            [7, 4, 3, 7],
+        ];
+    }
+}
